ced979f4aa57f8e9003bb9d47fc739b9cf2f7883
Nils Björklin <nils.bjorklin@gmail.com>
2024-12-04 11:27:54 +0100

    Added logic for reading input data
    Signed-off-by: Nils Björklin <nils.bjorklin@gmail.com>

--- src/days/day1/script.py
+++ src/days/day1/script.py
@@ -2,7 +2,16 @@ from src.days.template import Template
 
 
 def fetch_data():
-    return [3, 5, 6, 1], [3, 5, 6, 1]
+    f = open('data/days/day1/data', 'r')
+    data = ([], [])
+    for (i, line) in enumerate(f.readlines()):
+        parts = line.strip().split()
+        if len(parts) != 2:
+            raise ValueError('Invalid row at %s with value "%s"' % (i, line))
+        else:
+            data[0].append(int(parts[0]))
+            data[1].append(int(parts[1]))
+    return data
 
 
 def sort_data(data):
@@ -10,14 +19,15 @@ def sort_data(data):
 
 
 class Day1(Template):
-    def __init__(self, data=fetch_data()):
+    def __init__(self, data=0):
+        if data == 0:
+            data = fetch_data()
         super().__init__(1, sort_data(data), self.__func)
 
     def __func(self):
         list_1 = self.data[0]
         list_2 = self.data[1]
         diff = 0
-        print(list_1, list_2)
         for i in range(len(list_1)):
             diff += abs(list_1[i] - list_2[i])
         return diff
