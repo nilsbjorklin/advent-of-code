c066c1ed6dfe5ee83f834cf3be746b71c475e9ab
Nils Björklin <nils.bjorklin@gmail.com>
2024-12-07 17:27:43 +0100

    Day 7 part 2 v1
    Signed-off-by: Nils Björklin <nils.bjorklin@gmail.com>

--- src/days/day_7.py
+++ src/days/day_7.py
@@ -1,22 +1,35 @@
 import itertools
+from typing import Iterable
 
 from src.days.template import Template
 
 
 def parse_data(data):
     result = []
-    operators = {}
     max_number_of_values = 0
     for row in data:
         total, values = row.split(':')
         values = [int(num) for num in values.strip().split(' ')]
         max_number_of_values = max(max_number_of_values, len(values))
         result.append((int(total), values))
-    for i in range(1, max_number_of_values):
-        operators[i + 1] = []
-        for operator in list(itertools.product('*+', repeat=i)):
-            operators[i + 1].append(list(operator))
-    return result, operators
+    return result, max_number_of_values
+
+
+def operator_combinations(keys: range, options: Iterable):
+    result = {}
+    for i in keys:
+        result[i + 1] = []
+        for operator in list(itertools.product(options, repeat=i)):
+            result[i + 1].append(list(operator))
+    return result
+
+
+def calculate(row, operators):
+    target, values = row
+    if values[0] > target: return 0
+    if not operators: return values[0]
+
+    return calculate((target, [operators[0](values[0], values[1])] + values[2:]), operators[1:])
 
 
 class Day7(Template):
@@ -35,27 +48,27 @@ class Day7(Template):
 class Part1(Day7):
     def __init__(self, data=0):
         super().__init__(self.__func, data)
+        self.combs = operator_combinations(range(1, self.data[1]), [lambda a, b: a + b, lambda a, b: a * b])
 
     def __func(self):
-        operators_combinations_list = self.data[1]
         result = 0
         for row in self.data[0]:
-            result += next((res for operators in operators_combinations_list[len(row[1])] if
-                            (res := self.calculate(row, operators)) == row[0]), 0)
+            result += next((res for comb in self.combs[len(row[1])] if (res := calculate(row, comb)) == row[0]), 0)
         return result
-
-    def calculate(self, row, operators):
-        target, values = row
-
-        if values[0] > target: return 0
-        if not operators: return values[0]
-
-        return self.calculate((target, [eval(f"{values[0]} {operators[0]} {values[1]}")] + values[2:]), operators[1:])
 
 
 class Part2(Day7):
     def __init__(self, data=0):
         super().__init__(self.__func, data)
+        self.combs = operator_combinations(range(1, self.data[1]), [
+            lambda a, b: a + b,
+            lambda a, b: a * b,
+            lambda a, b: int(f"{a}{b}")
+        ])
 
     def __func(self):
-        pass
+        result = 0
+
+        for row in self.data[0][:50]:
+            result += next((res for comb in self.combs[len(row[1])] if (res := calculate(row, comb)) == row[0]), 0)
+        return result
