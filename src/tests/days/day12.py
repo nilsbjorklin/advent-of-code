01ec0c83d0a1609e358ce664e6c9f35ff893de33
Nils Björklin <nils.bjorklin@gmail.com>
2024-12-13 00:24:27 +0100

    Solution day 12 part 2
    Signed-off-by: Nils Björklin <nils.bjorklin@gmail.com>

--- src/tests/days/day12.py
+++ src/tests/days/day12.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 from src.days.day12.part_1 import run as part_1_run
+from src.days.day12.part_2 import run as part_2_run
 
 test_data_mini = '''
 AAAA
@@ -17,6 +18,23 @@ OXOXO
 OOOOO
 '''
 
+test_data_mini_3 = '''
+EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE
+'''
+
+test_data_mini_4 = '''
+AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA
+'''
+
 test_data = '''
 RRRRIICCFF
 RRRRIICCCF
@@ -31,7 +49,7 @@ MMMISSJEEE
 '''
 
 
-class Test(TestCase):
+class TestPart1(TestCase):
     def test_part_1_mini_1(self):
         self.assertEqual(140, part_1_run(test_data_mini.strip().splitlines()))
 
@@ -40,3 +58,20 @@ class Test(TestCase):
 
     def test_part_1(self):
         self.assertEqual(1930, part_1_run(test_data.strip().splitlines()))
+
+
+class TestPart2(TestCase):
+    def test_part_2_mini_1(self):
+        self.assertEqual(80, part_2_run(test_data_mini.strip().splitlines()))
+
+    def test_part_2_mini_2(self):
+        self.assertEqual(436, part_2_run(test_data_mini_2.strip().splitlines()))
+
+    def test_part_2_mini_3(self):
+        self.assertEqual(236, part_2_run(test_data_mini_3.strip().splitlines()))
+
+    def test_part_2_mini_4(self):
+        self.assertEqual(368, part_2_run(test_data_mini_4.strip().splitlines()))
+
+    def test_part_2(self):
+        self.assertEqual(1206, part_2_run(test_data.strip().splitlines()))
