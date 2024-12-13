84fb3ae3f3f95499c9a1c3cde4fb0a54eae6674a
Nils Björklin <nils.bjorklin@gmail.com>
2024-12-13 13:39:28 +0100

    Solution day 13 part 1
    Signed-off-by: Nils Björklin <nils.bjorklin@gmail.com>

---
+++ src/tests/days/day13.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+
+from src.days.day13.part_1 import run as part_1_run
+
+test_data_mini = '''
+Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+'''
+
+test_data = '''
+Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279
+'''
+
+
+class TestPart1(TestCase):
+    def test_part_1_mini_1(self):
+        self.assertEqual(280, part_1_run(test_data_mini))
+
+    def test_part_1(self):
+        self.assertEqual(480, part_1_run(test_data))
