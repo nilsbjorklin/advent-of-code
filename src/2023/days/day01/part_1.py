8a27906b6f80eddaafdc096da11a22793dd38bad
Nils Björklin <nils.bjorklin@gmail.com>
2024-12-15 12:23:25 +0100

    Solution 2023 day01 part 1
    Signed-off-by: Nils Björklin <nils.bjorklin@gmail.com>

---
+++ src/2023/days/day01/part_1.py
@@ -0,0 +1,20 @@
+import re
+
+
+def read_data(input_data):
+    result = []
+    for line in input_data:
+        result.append(re.compile(r'\d').findall(line))
+    return result
+
+
+def run(input_data: list[str]):
+    data = read_data(input_data)
+    result = 0
+    for row in data:
+        result += int(row[0] + row[len(row) - 1])
+    return result
+
+
+if __name__ == '__main__':
+    print(run(open('../../data/days/01/data', 'r').readlines()))
