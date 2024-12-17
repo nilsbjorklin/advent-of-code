22a21b0159ddcef677e28e8c1879b3a62392690b
Nils Björklin <nils.bjorklin@gmail.com>
2024-12-17 11:35:58 +0100

    Solution day 16 part 1 and part 2
    Signed-off-by: Nils Björklin <nils.bjorklin@gmail.com>

---
+++ src/2024/tests/test_day16.py
@@ -0,0 +1,56 @@
+from importlib import import_module
+from unittest import TestCase
+
+part_1 = import_module('src.2024.days.day16.part_1')
+part_2 = import_module('src.2024.days.day16.part_2')
+
+test_data_mini = '''
+###############
+#.......#....E#
+#.#.###.#.###.#
+#.....#.#...#.#
+#.###.#####.#.#
+#.#.#.......#.#
+#.#.#####.###.#
+#...........#.#
+###.#.#####.#.#
+#...#.....#.#.#
+#.#.#.###.#.#.#
+#.....#...#.#.#
+#.###.#.#.#.#.#
+#S..#.....#...#
+###############'''
+
+test_data = '''
+#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################
+'''
+
+
+class TestPart1(TestCase):
+    def test_part_1_mini(self):
+        self.assertEqual(7036, part_1.run(test_data_mini))
+
+    def test_part_1(self):
+        self.assertEqual(11048, part_1.run(test_data))
+
+    def test_part_2_mini(self):
+        self.assertEqual(45, part_2.run(test_data_mini))
+
+    def test_part_2(self):
+        self.assertEqual(64, part_2.run(test_data))
