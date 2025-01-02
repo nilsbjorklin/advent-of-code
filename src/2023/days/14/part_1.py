09a11e461ea5d7be2114734a99d04c489583bf26
Nils Björklin <nils.bjorklin@gmail.com>
2025-01-02 03:13:56 +0100

    Removes unused variables

--- src/2023/days/14/part_1.py
+++ src/2023/days/14/part_1.py
@@ -7,15 +7,11 @@ def run(input_data):
     rows = read_data(input_data)
     floor = defaultdict(lambda: 0)
     result = 0
-    stones = []
-    pillars = []
     for row_idx, row in enumerate(rows):
         for col_idx, char in enumerate(row):
             if char == '#':
-                floor[col_idx] = row_idx + 1
-                pillars.append((row_idx, col_idx))                    
+                floor[col_idx] = row_idx + 1                  
             elif char == 'O':                    
-                stones.append((floor[col_idx], col_idx))
                 result += len(rows) - floor[col_idx]
                 floor[col_idx] += 1
     return result
