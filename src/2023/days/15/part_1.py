9151d644e5d8dbcc24fe0971ed068ac06bcd520a
Nils Björklin <nils.bjorklin@gmail.com>
2025-01-02 05:00:56 +0100

    2023: Solution day 15 part 1

---
+++ src/2023/days/15/part_1.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+def read_data(input_data):
+    return input_data.split(",")
+
+def run(input_data):
+    rows = read_data(input_data)
+    total_sum = 0
+    for row in rows:
+        result = 0
+        for char in row:
+            result += ord(char)
+            result *= 17
+            result = result % 256
+        total_sum += result
+    return total_sum
+            
+
+if __name__ == "__main__":
+    print(run(open("src/2023/data/days/15/data", "r").read()))
