a86e934277a651a1355ccebcd7d4ee85bab59e26
Nils Björklin <nils.bjorklin@gmail.com>
2025-01-03 04:23:15 +0100

    Some refactoring

--- src/2023/days/21/part_1.py
+++ src/2023/days/21/part_1.py
@@ -3,9 +3,6 @@ from collections import deque
 
 directions = [1j, -1j, 1, -1]
 
-ends = set()
-visited = set()
-
 def read_data(input_data):
     start = 0
     plots= []
@@ -20,33 +17,21 @@ def read_data(input_data):
 def run(input_data, max_steps):
     start, plots = read_data(input_data)
     positions = deque([[0, start]])
+    ends = set()
+    visited = set() 
     while len(positions) != 0:
         step, pos = positions.popleft()
         if step > max_steps:
             break
-        print(step, pos, end="")
         if step % 2 == 0:
             ends.add(pos)
         if pos not in visited:
             visited.add(pos)
-            for first in directions:
-                first_pos = pos + first
-                if first_pos in plots and first_pos not in visited:
-                    if (step + 1, first_pos) not in positions:
-                        positions.append((step + 1, first_pos))
-    print_grid(start, ends, plots)
+            for direction in directions:
+                next_pos = pos + direction
+                if next_pos in plots and next_pos not in visited:
+                    positions.append((step + 1, next_pos))
     return len(ends)
-def print_grid(start, ends, plots):
-    for y in range(140):
-        for x in range(140):
-            pos = complex(x, y)
-            if pos not in plots:
-                print('#', end="")
-            elif pos in ends:  
-                print('O', end="") 
-            else:
-                print('.', end="") 
-        print()
 
 
 if __name__ == "__main__":
