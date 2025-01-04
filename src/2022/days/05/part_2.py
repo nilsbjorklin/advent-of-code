900d1d4174bf1840fe1d805e9fd5ae908ef90ce3
Nils Björklin <nils.bjorklin@gmail.com>
2025-01-04 06:45:42 +0100

    2022: Solution day 5 part 2

--- src/2022/days/05/part_2.py
+++ src/2022/days/05/part_2.py
@@ -1,4 +1,3 @@
-from collections import deque
 import re
 
 
@@ -12,7 +11,7 @@ def read_data(input_data):
     for row in stack_rows[::-1]:
         if not indexes:
             indexes = [match.span()[0] for match in digit.finditer(row)]
-            stacks = [deque() for _ in indexes]
+            stacks = [[] for _ in indexes]
             continue
         for num, char in enumerate([row[i] for i in indexes]):
             if char.isalpha():
@@ -20,18 +19,18 @@ def read_data(input_data):
 
     move_rows = move_input.splitlines()
     moves = [
-        (start - 1, dest - 1)
-        for repeat, start, dest in [map(int, digit.findall(row)) for row in move_rows]
-        for _ in range(repeat)
+        (amount, start - 1, dest - 1)
+        for amount, start, dest in [map(int, digit.findall(row)) for row in move_rows]
     ]
     return stacks, moves
 
 
 def run(input_data):
     stacks, moves = read_data(input_data)
-    for start, dest in moves:
-        stacks[dest].append(stacks[start].pop())
-    return "".join([stack.pop() for stack in stacks])
+    for amount, start, dest in moves:
+        stacks[dest] += stacks[start][-amount:]
+        stacks[start] = stacks[start][:-amount]
+    return "".join([stack[-1] for stack in stacks])
 
 
 if __name__ == "__main__":
