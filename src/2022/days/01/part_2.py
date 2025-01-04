50345078fcac2ca3ca5927363afa1cd9869eedfc
Nils Björklin <nils.bjorklin@gmail.com>
2025-01-04 02:55:02 +0100

    2022: Solution day 1 part 2

---
+++ src/2022/days/01/part_2.py
@@ -0,0 +1,15 @@
+def read_data(input_data):
+    data = []
+    for section in input_data.split("\n\n"):
+        rows = []
+        for row in section.strip().splitlines():
+            rows.append(int(row))
+        data.append(sum(rows))
+    return data
+
+def run(input_data: list[str]):
+    data = read_data(input_data)
+    return sum(sorted(data, reverse=True)[:3])
+
+if __name__ == "__main__":
+    print(run(open("src/2022/data/days/01/data", "r").read()))
