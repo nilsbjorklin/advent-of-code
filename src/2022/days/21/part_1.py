86a6b924f65c933007ed55a8c8798ab1b7251210
Nils Björklin <nils.bjorklin@gmail.com>
2025-01-12 15:09:48 +0100

    2022: Solution day 21 part 1

---
+++ src/2022/days/21/part_1.py
@@ -0,0 +1,49 @@
+from functools import lru_cache
+import re
+
+monkey_actions = {}
+
+
+def read_data(input_data):
+    pattern = re.compile(r"(\w+) ([\+\-\*\/]) (\w+)")
+    global monkey_actions
+    for row in input_data.splitlines():
+        name, action = row.split(":")
+        actions = pattern.findall(action)
+        if len(actions) != 0:
+            monkey_actions[name] = actions[0]
+        else:
+            monkey_actions[name] = int(action)
+
+
+def run(input_data):
+    read_data(input_data)
+    return calculate_monkey("root")
+
+
+@lru_cache
+def calculate_monkey(name):
+    actions = monkey_actions[name]
+    if type(actions) != tuple:
+        return actions
+    result = process_values(
+        calculate_monkey(actions[0]), calculate_monkey(actions[2]), actions[1]
+    )
+    return result
+
+
+@lru_cache
+def process_values(first, second, operator):
+    match operator:
+        case "+":
+            return first + second
+        case "-":
+            return first - second
+        case "*":
+            return first * second
+        case "/":
+            return first // second
+
+
+if __name__ == "__main__":
+    print(run(open("src/2022/data/days/21/data", "r").read()))
