0b8c863e4f9fc9bd99b899cf91d4da9b29f59add
Nils Björklin <nils.bjorklin@gmail.com>
2025-01-25 13:24:40 +0100

    2021: Solution day 5 part 2

---
+++ src/2021/days/05/part_2.py
@@ -0,0 +1,39 @@
+from collections import defaultdict
+
+
+def read_data(input_data):
+    lines = []
+    for line in input_data.splitlines():
+        start, end = line.split("->")
+        lines.append((complex(*map(int, start.strip().split(","))), complex(*map(int, end.strip().split(",")))))
+    return lines
+
+
+def run(input_data: str):
+    lines = read_data(input_data)
+    lines_in_pos = defaultdict(int)
+    for start, end in lines:
+        increment = calculate_increment(start, end)
+        if increment is not None:
+            lines_in_pos[start]+=1
+            while start - end != 0:
+                lines_in_pos[end]+=1
+                end += increment
+    return len(list(filter(lambda x: x > 1, lines_in_pos.values())))
+
+
+def calculate_increment(start, end):
+    diff = start - end
+    if diff.real == 0 or diff.imag == 0:
+        return diff_change(diff)
+    elif abs(diff.real) == abs(diff.imag):
+        return diff_change(complex(diff.real, 0)) + diff_change(complex(0, diff.imag))
+def diff_change(diff):
+    if diff.real == 0 or diff.imag == 0:
+        if diff.real != 0:
+            return 1 if diff.real > 0 else -1
+        if diff.imag != 0:
+            return  1j if diff.imag > 0 else -1j
+
+if __name__ == "__main__":
+    print(run(open("src/2021/data/days/05/data", "r").read()))
