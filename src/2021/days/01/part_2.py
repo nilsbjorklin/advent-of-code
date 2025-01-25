576d1eb4f59e7c50c1c319f310ee7ed9bf3ba84f
Nils Björklin <nils.bjorklin@gmail.com>
2025-01-25 10:23:33 +0100

    2021: Solution day 1 part 2

---
+++ src/2021/days/01/part_2.py
@@ -0,0 +1,12 @@
+def read_data(input_data):
+    return [int(row.strip()) for row in input_data.splitlines()] 
+
+def run(input_data: str):
+    values = read_data(input_data)
+    values = [values[index] + values[index + 1] + values[index + 2] for index in range(len(values) - 2)]
+    return sum([1 if values[index] < values[index + 1] else 0 for index in range(len(values) - 1)])
+        
+
+
+if __name__ == "__main__":
+    print(run(open("src/2021/data/days/01/data", "r").read()))
