c31c36231620b5331020eec6220c9c85cf64cf24
Nils Björklin <nils.bjorklin@gmail.com>
2025-01-26 13:15:03 +0100

    2021: Solution day 10 part 1

--- src/2021/days/10/part_1.py
+++ src/2021/days/10/part_1.py
@@ -1,27 +1,21 @@
-chunk_start = {"(", "[", "{", "<"}
-chunk_ends = {"(": ")", "[": "]", "{": "}", "<": ">"}
-point_table = {")": 3, "]": 57, "}": 1197, ">": 25137}
-
-
 def read_data(input_data):
-    return [list(line) for line in input_data.splitlines()]
+    point_map = {"(": 3, ")": -3, "[": 57, "]": -57, "{": 1197, "}": -1197, "<": 25137, ">": -25137}
+    return [[point_map[char] for char in line] for line in input_data.splitlines()]
 
 
 def run(input_data: str):
     lines = read_data(input_data)
-    result = 0
-    for line in lines:
-        opening_brackets = []
-        for char in line:
-            if char in chunk_start:
-                opening_brackets.append(char)
-            else:
-                expected = chunk_ends[opening_brackets.pop(-1)]
-                if expected != char:
-                    result += point_table[char]
-                    break
-    return result
+    return sum([line_result(line) for line in lines])
 
+def line_result(line):
+    values = []
+    for val in line:
+        if val > 0:
+            values.append(val)
+        else:
+            if values.pop(-1) + val != 0:
+                return -val
+    return 0
 
 if __name__ == "__main__":
     print(run(open("src/2021/data/days/10/data", "r").read()))
