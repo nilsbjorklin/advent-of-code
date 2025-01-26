6dfc40b1ef95aba5aea65857bf4755bdabf176af
Nils Björklin <nils.bjorklin@gmail.com>
2025-01-26 12:19:57 +0100

    2021: Solution day 9 part 1

--- src/2021/days/09/part_1.py
+++ src/2021/days/09/part_1.py
@@ -10,14 +10,15 @@ def read_data(input_data):
 
 def run(input_data: str):
     values = read_data(input_data)
-    return sum([value+1 for pos, value in values.items() if check_low_point(pos, value, values)])
+    return sum([check_low_point(pos, values) for pos in values.keys()])
 
-def check_low_point(pos, value, values):
+def check_low_point(pos, values):
+    value = values[pos]
     for direction in directions:
         neighbor = pos + direction
         if neighbor in values and value >= values[neighbor]:
-            return False
-    return True
+            return 0
+    return value + 1
 
 if __name__ == "__main__":
     print(run(open("src/2021/data/days/09/data", "r").read()))
