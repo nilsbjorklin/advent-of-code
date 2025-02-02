61d75e0c88173c07b38e03ec235de7fe34dce652
Nils Björklin <nils.bjorklin@gmail.com>
2025-02-02 10:25:52 +0100

    2021: Solution day 14 part 1

--- src/2021/days/14/part_1.py
+++ src/2021/days/14/part_1.py
@@ -1,7 +1,9 @@
 def read_data(input_data):
     template, insertions = input_data.split("\n\n")
     insertions = [insertion.split("->") for insertion in insertions.splitlines()]
-    insertions = {tuple(pattern.strip()): char.split()[0] for pattern, char in insertions}
+    insertions = {
+        tuple(pattern.strip()): char.split()[0] for pattern, char in insertions
+    }
     return template, insertions
 
 
@@ -10,11 +12,12 @@ def run(input_data):
     for _ in range(10):
         new_template = template[0]
         for index in range(len(template) - 1):
-            tuple_2 = (template[index], template[index  +1])
-            if tuple_2 in insertions.keys():
-                new_template += insertions[tuple_2] + template[index + 1]
+            first = template[index]
+            second = template[index + 1]
+            if (first, second) in insertions.keys():
+                new_template += insertions[(first, second)] + second
         template = new_template
-        
+
     least_common = most_common = None
     for char in set(template):
         if least_common is None:
@@ -23,6 +26,7 @@ def run(input_data):
             least_common = min(template.count(char), least_common)
             most_common = max(template.count(char), most_common)
     return most_common - least_common
+
 
 if __name__ == "__main__":
     print(run(open("src/2021/data/days/14/data", "r").read()))
