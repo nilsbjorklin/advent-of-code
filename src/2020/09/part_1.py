d7a4424e4ab9fde69cee81dd6f434c90b65dab00
Nils Björklin <nils.bjorklin@gmail.com>
2025-02-09 18:20:26 +0100

    2020: Solution day 9 part 1
    Signed-off-by: Nils Björklin <nils.bjorklin@gmail.com>

---
+++ src/2020/09/part_1.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+
+def read_data(input_data: str) -> list[int]:
+    return list(map(int, input_data.splitlines()))
+
+
+def run(input_data: str, last_nums) -> int:
+    numbers: list[int] = read_data(input_data)
+    index = last_nums
+    last_numbers = deque(numbers[:last_nums], maxlen=last_nums)
+    for index in range(index, len(numbers)):
+        number = numbers[index]
+        if number_valid(number, last_numbers):
+            last_numbers.append(number)
+        else:
+            return number
+
+
+def number_valid(target, last_numbers):
+    last_numbers_sorted = sorted(last_numbers)
+    for index, first_number in enumerate(last_numbers_sorted):
+        if first_number > target:
+            return False
+        for second_index in range(index + 1, len(last_numbers_sorted)):
+            total = last_numbers_sorted[second_index] + first_number
+            diff = total - target
+            if diff == 0:
+                return True
+            elif diff > 0:
+                break
+
+
+if __name__ == "__main__":
+    print(run(open("data", "r").read(), 25))
