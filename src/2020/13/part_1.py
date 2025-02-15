b62e5c4acf9a826da56ed9bd0e9601e4bedecd41
Nils Björklin <nils.bjorklin@gmail.com>
2025-02-15 11:28:03 +0100

    2020: Solution day 13 part 1
    Signed-off-by: Nils Björklin <nils.bjorklin@gmail.com>

---
+++ src/2020/13/part_1.py
@@ -0,0 +1,20 @@
+def read_data(input_data: str) -> (int, list):
+    first, second = input_data.splitlines()
+    return int(first), [int(num) for num in second.split(",") if num != "x"]
+
+
+def run(input_data: str) -> int:
+    earliest, schedule = read_data(input_data)
+    values = {calc_wait(earliest, num): num for num in schedule}
+    min_wait = min(values.keys())
+    return min_wait * values[min_wait]
+
+
+def calc_wait(earliest, num):
+    times = earliest // num + 1
+    num *= times
+    return num - earliest
+
+
+if __name__ == "__main__":
+    print(run(open("data", "r").read()))
