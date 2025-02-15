d15b1f5f9d0483aaf52757a2d3a96b873430475f
Nils Björklin <nils.bjorklin@gmail.com>
2025-02-15 15:17:21 +0100

    2020: Solution day 16 part 1
    Signed-off-by: Nils Björklin <nils.bjorklin@gmail.com>

---
+++ src/2020/16/part_1.py
@@ -0,0 +1,37 @@
+import re
+from functools import lru_cache
+
+rules = []
+
+
+def read_data(input_data: str) -> (list, list, list):
+    global rules
+    rules, _, tickets = input_data.split("\n\n")
+    pattern = re.compile(r"(\d+)-(\d+)")
+    rules = [
+        (int(min_value), int(max_value))
+        for min_value, max_value in pattern.findall(rules)
+    ]
+    return [list(map(int, line.split(","))) for line in tickets.splitlines()[1:]]
+
+
+def run(input_data: str) -> int:
+    tickets = read_data(input_data)
+    invalid_numbers_sum = 0
+    for ticket in tickets:
+        for number in ticket:
+            if not is_valid(number):
+                invalid_numbers_sum += number
+    return invalid_numbers_sum
+
+
+@lru_cache
+def is_valid(number):
+    for min_value, max_value in rules:
+        if min_value <= number <= max_value:
+            return True
+    return False
+
+
+if __name__ == "__main__":
+    print(run(open("data", "r").read()))
