ee3e8b7787c139f3967ef31df39848fe258b8156
Nils Björklin <nils.bjorklin@gmail.com>
2025-02-22 15:14:03 +0100

    2010: Solution day 6 part 1
    Signed-off-by: Nils Björklin <nils.bjorklin@gmail.com>

---
+++ src/2019/06/part_1.py
@@ -0,0 +1,27 @@
+from black.trans import defaultdict
+
+connections = defaultdict(list)
+connections_count = defaultdict(int)
+
+
+def read_data(input_data):
+    global connections
+    for value in input_data.splitlines():
+        center, orbiter = value.split(")")
+        connections[center].append(orbiter)
+
+
+def run(input_data) -> int:
+    read_data(input_data)
+    check_connections()
+    return sum(connections_count.values())
+
+
+def check_connections(start="COM", depth=0):
+    connections_count[start] += depth
+    for connected in connections[start]:
+        check_connections(connected, depth + 1)
+
+
+if __name__ == "__main__":
+    print(run(open("data").read()))
