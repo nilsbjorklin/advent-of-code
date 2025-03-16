dfa41fc8f80c19970372eb215e3fb51a2cbd5368
Nils Björklin <nils.bjorklin@gmail.com>
2025-03-16 14:26:37 +0100

    2018 Day 2 part 2
    Signed-off-by: Nils Björklin <nils.bjorklin@gmail.com>

--- src/2018/02/part_2.py
+++ src/2018/02/part_2.py
@@ -6,21 +6,17 @@ def run(input_data) -> int:
     values = read_data(input_data)
     for first_index, first_value in enumerate(values):
         for second_index in range(first_index + 1, len(values)):
-            second_value = values[second_index]
-            result = check_missmatch(first_value, second_value)
+            result = check_missmatch(first_value, values[second_index])
             if result:
                 return result
 
 
 def check_missmatch(first_value, second_value):
-    invalid_counter = 0
     result = ""
     for char_index, first_char in enumerate(first_value):
-        if first_char != second_value[char_index]:
-            invalid_counter += 1
-        else:
+        if first_char == second_value[char_index]:
             result += first_char
-        if invalid_counter > 1:
+        if len(result) < char_index:
             return False
     return result
 
